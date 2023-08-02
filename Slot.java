152d2c2d58852834e142865ef857a18e42eabbe7
Giancarlo A. Flores <136430725+gyanplores@users.noreply.github.com>
2023-08-03 05:44:18 +0800

    Update Slot.java

--- Slot.java
+++ Slot.java
@@ -31,6 +31,21 @@ public class Slot {
 
     }
 
+    public void restockItem(int amount){
+        int i;
+
+        for(i = 0 ; i < amount ; i++){
+            items.add(getItem());
+        }
+    }
+
+    /**
+     * This method removes an item from the slot.
+     */
+    public void removeItem( ){
+        items.remove(items.size()-1);
+    }
+
     /**
      * This method is responsible for checking if the slot is empty.
      * @return True if slot is empty, false otherwise.
@@ -43,9 +58,17 @@ public class Slot {
 
     /**
      * This method returns the item.
-     * @return Item inside the slot.
+     * @return Item inside the slot, null if none.
      */
-    public Item getItem(){ return items.get(0); }
+    public Item getItem(){
+        if(items.size() > 0){
+            return items.get(0);
+        }
+        else{
+            return null;
+        }
+    }
+
 
     /**
      * This method returns the amount of items inside the slot.
