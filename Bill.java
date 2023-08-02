6a5a4fc5e8cf03d34e8d10336c7f33ba7ab9daad
Giancarlo A. Flores <136430725+gyanplores@users.noreply.github.com>
2023-08-03 05:43:38 +0800

    Update Bill.java

--- Bill.java
+++ Bill.java
@@ -7,16 +7,21 @@ public class Bill {
 
     /**
      * The constructor is responsible for assigning a value to a bill.
+     *
      * @param value The amount in terms of value.
      */
-    Bill(float value){
+    Bill(float value) {
         this.value = value;
     }
 
     /**
      * This method returns the value of the bill.
+     *
      * @return Bill value.
      */
-    public double getValue(){return value;}
+    public double getValue() {
+        return value;
+    }
+
     private double value;
 }
